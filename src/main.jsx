131d896fad17586b2757a485549e87f127de1b8a
HaqueArif <haquearif143@gmail.com>
2023-06-07 06:05:10 +0600

    installed and setup  firebase

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes';
+import AuthProvider from './providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
