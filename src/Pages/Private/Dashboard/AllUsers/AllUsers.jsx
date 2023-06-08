1fd888eaa42b74742f6ed63cd7ee5606d01e5e91
HaqueArif <haquearif143@gmail.com>
2023-06-09 03:16:31 +0600

    created make admin systems

--- src/Pages/Private/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Private/Dashboard/AllUsers/AllUsers.jsx
@@ -1,20 +1,41 @@
 import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
+
 
     const { data: users = [], refetch } = useQuery(['users'], async () => {
         const res = await fetch('http://localhost:5000/users')
         return res.json();
     })
 
+    const handleMakeAdmin = user =>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`,{
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+            if(data.modifiedCount){
+                refetch()
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
+    
     return (
         <div>
             <h2>{users.length}</h2>
 
             <div className="overflow-x-auto">
                 <table className="table">
-                    {/* head */}
                     <thead>
                         <tr>
                             <th>#</th>
@@ -31,10 +52,14 @@ const AllUsers = () => {
                             <th>{i + 1}</th>
                             <td>{user.name}</td>
                             <td>{user.email}</td>
-                            
+
                             <td>{user.role === 'admin' ? 'Admin' : user.role === 'instructor' ? 'Instructor' : 'User'}</td>
 
-                            <td><button className="btn">Delete</button></td>
+                            <td className="flex flex-col gap-2">
+                                <button onClick={()=>handleMakeAdmin(user)} disabled={user.role === 'admin'} className="btn btn-sm">Admin</button>
+                                <button onClick={()=>handleMakeAdmin(user)} disabled={user.role === 'instructor'} className="btn btn-sm">Instructor</button>
+                            
+                            </td>
                             
                         </tr>  )
                         }
